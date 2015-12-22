ab2dba96a87a10bec8c6c0bfae03d64261831ac2
esqu1 <brandbest1@gmail.com>
2015-12-22 10:42:26 -0500

    .emacs.d

---
+++ emacs/.emacs.d/snippets/java-mode/test.java
@@ -0,0 +1,3 @@
+BufferedReader br = new BufferedReader(new FileReader());
+PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter()));
+StringTokenizer st = new StringTokenizer(br.readLine());
