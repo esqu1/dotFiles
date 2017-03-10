e8e8386222ff9fe8ef3d2cfced5764434d52f63c
esqu1 <brandbest1@gmail.com>
2017-03-10 00:47:52 -0500

    remove .emacs.d, update latex

--- emacs/.emacs.d/README.md
+++
@@ -1,4 +0,0 @@
-Emacs configuration
-===
-
-This has my various emacs config files. I haven't bothered to sort the clutter yet for the airline themes, but you can look at the other ones. x
--- emacs/.emacs.d/snippets/java-mode/test.java
+++
@@ -1,3 +0,0 @@
-BufferedReader br = new BufferedReader(new FileReader());
-PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter()));
-StringTokenizer st = new StringTokenizer(br.readLine());
